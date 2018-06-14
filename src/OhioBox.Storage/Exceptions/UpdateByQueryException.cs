7ce8eb4ca5d61541d524a83fcef14a91e1e64631
ysa23 <ysa.angel@gmail.com>
2018-06-14 15:37:34 +0300

    Add exceptions to contracts

---
+++ src/OhioBox.Storage/Exceptions/UpdateByQueryException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OhioBox.Storage.Exceptions
+{
+	public class UpdateByQueryException : Exception
+	{
+		public UpdateByQueryException(string message) : base($"Unable to update by query: {message}")
+		{
+		}
+	}
+}
